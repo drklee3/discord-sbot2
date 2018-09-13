5208ca5fb1db972acab46c5687f519da524d1798
drklee3 <dlee3@scu.edu>
2018-09-13 13:52:41 -0700

    Use #

--- src/commands/meta.rs
+++ src/commands/meta.rs
@@ -259,7 +259,7 @@ command!(stats(ctx, msg) {
         m.embed(|e| e
             .color(0x3498db)
             .title(&format!(
-                "sushii v{} - Build {} ({} ID {})",
+                "sushii v{} - Build {} ({} #{})",
                 bot_version, build_number.unwrap_or("N/A"),
                 agent_name.unwrap_or("N/A"), agent_id.unwrap_or("N/A")
             ))
